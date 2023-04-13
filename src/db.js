9eecc9d6622ccebe4e79832db696073beb6b5b9a
Pablo Braga <pablofelix567@gmail.com>
2023-04-13 15:46:02 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const sequelize = require('sequelize');
 const database = new sequelize('pointfairdb','USUARIO','SENHA',{
-    dialect: 'mysql', host:'localhost', port:3306
+    dialect: 'mysql', host:'HOST', port:3306
 });
 database.sync();
 module.exports = database;
