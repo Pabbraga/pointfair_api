aad91c91cd1a06eacd9831a69855ad5df2fa40aa
Pabbraga <pablofelix567@gmail.com>
2023-05-20 21:19:56 -0300

    desatrelando doc seller

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,6 @@ const routes = express.Router()
 
 import authRoutes from "./auth.js";
 import usersRoutes from "./users.js";
-// import sellersRoutes from "./sellers.js";
 import addressRoutes from "./address.js";
 import fairRoutes from "./fair.js";
 import publicationRoutes from "./publication.js";
@@ -12,7 +11,6 @@ import pictureRoutes from "./picture.js";
 
 routes.use("/auth", authRoutes);
 routes.use("/user", usersRoutes);
-// routes.use("/seller", sellersRoutes);
 routes.use("/fair", fairRoutes);
 routes.use("/address", addressRoutes);
 routes.use("/publication", publicationRoutes);
