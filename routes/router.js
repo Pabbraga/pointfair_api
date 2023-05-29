a6183bdba963e05494d5fb64a82d54f45e92c625
Pabbraga <pablofelix567@gmail.com>
2023-05-29 20:25:36 -0300

    Revert "correção"

--- routes/router.js
+++ routes/router.js
@@ -8,9 +8,7 @@ import addressRoutes from "./address.js";
 import fairRoutes from "./fair.js";
 import publicationRoutes from "./publication.js";
 import pictureRoutes from "./picture.js";
-import testeRoutes from "./teste.js";
 
-routes.use("/", testeRoutes);
 routes.use("/auth", authRoutes);
 routes.use("/user", usersRoutes);
 routes.use("/fair", fairRoutes);
--- routes/teste.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import TesteController from "../controllers/testeController.js";
-
-const router = express.Router();
-
-router.get('/', TesteController.teste);
-
-export default router;
