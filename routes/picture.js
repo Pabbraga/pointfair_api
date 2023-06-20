080e41efcfc3d7a7508314dc2da3786584e7ef17
Pabbraga <pablofelix567@gmail.com>
2023-06-19 21:14:15 -0300

    teste

--- routes/picture.js
+++ routes/picture.js
@@ -5,5 +5,7 @@ import upload from "../config/multer.js";
 const router = express.Router();
 
 router.post('/upload', upload.single("file"), pictureController.upload);
+router.get('/:id', pictureController.get);
+router.delete('/:id', pictureController.delete);
 
 export default router;
