628b18f1959943207c1b1ee1c25ddfdd87cd603a
ghsiqueira <gabrielh2805@gmail.com>
2023-06-20 18:02:14 -0300

    fazer na etec esqueci senha

---
+++ controllers/passwordController.js
@@ -0,0 +1,39 @@
+import bcrypt from "bcrypt";
+import { User } from "../models/User.js";
+import nodemailer from "nodemailer";
+
+const passwordController = {
+  generateCode: async (req, res) => {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (!userExists) {
+      return res.status(404).json("Usuário não encontrado");
+    }
+    const passwordCode = Math.floor(100000 + Math.random() * 900000);
+
+    User.passwordCode = passwordCode;
+    await User.save();
+
+    try {
+      const transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+          user: "@mail.com",
+          pass: "senha",
+        },
+      });
+
+      const mailOptions = {
+        from: "@mail.com",
+        to: User.email,
+        subject: "Código de senha",
+        text: `Seu código de senha é: ${passwordCode}`,
+      };
+
+      await transporter.sendMail(mailOptions);
+      res.status(200).json("Código de senha gerado e enviado com sucesso");
+    } catch (error) {
+      console.error(error);
+      res.status(500).json("Erro ao enviar o e-mail");
+    }
+  },
+};
